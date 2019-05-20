22ba0821a7fbea5b539319d5336fb646111a479b
Sidclei Wilker <sidclei.freitas@urbana-pe.com.br>
2019-05-20 14:08:01 -0300

    Modelo estado e cidade mais repositorio de ambos

--- src/main/java/com/sidcleywilker/cursomc/CursoMcApplication.java
+++ src/main/java/com/sidcleywilker/cursomc/CursoMcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.sidcleywilker.cursomc.domain.Categoria;
+import com.sidcleywilker.cursomc.domain.Cidade;
+import com.sidcleywilker.cursomc.domain.Estado;
 import com.sidcleywilker.cursomc.domain.Produto;
 import com.sidcleywilker.cursomc.repositories.CategoriaRepository;
+import com.sidcleywilker.cursomc.repositories.CidadeRepository;
+import com.sidcleywilker.cursomc.repositories.EstadoRepository;
 import com.sidcleywilker.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursoMcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRep;
+	
+	@Autowired
+	private CidadeRepository cidaderiaRep;
+	
+	@Autowired
+	private EstadoRepository estadoRep;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
@@ -44,6 +54,21 @@ public class CursoMcApplication implements CommandLineRunner{
 		
 		categoriaRep.saveAll(Arrays.asList(c1,c2));
 		produtoRep.saveAll(Arrays.asList(p1,p2,p3));
+		
+		//====================================================
+		
+		Estado e1 = new Estado(null, "minas gerais");
+		Estado e2 = new Estado(null, "são paulo ");
+		
+		Cidade cid = new Cidade(null, "urbelandia", e1);
+		Cidade cid2 = new Cidade(null, "sao paulo", e2);
+		Cidade cid3 = new Cidade(null, "campinas", e2);
+		
+		e1.setCidades(Arrays.asList(cid));
+		e2.setCidades(Arrays.asList(cid2,cid3));
+		
+		estadoRep.saveAll(Arrays.asList(e1,e2));
+		cidaderiaRep.saveAll(Arrays.asList(cid,cid2,cid3));
 	}
 
 }
